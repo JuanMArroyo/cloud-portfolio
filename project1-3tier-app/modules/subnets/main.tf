6c7d1cdc0481fa466f4b66eb1949bd63c9d73531
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-07 11:45:45 -0400

    Update main.tf

--- project1-3tier-app/modules/subnets/main.tf
+++ project1-3tier-app/modules/subnets/main.tf
@@ -1,1 +1,89 @@
+#-----------------------------------
+# Public Subnets
+#-----------------------------------
+resource "aws_subnet" "public_a" {
+    vpc_id = var.vpc_id
+    cidr_block = var.az_a_cidr_public
+    availability_zone = var.az_a 
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-a"
+    }
+}
+
+resource "aws_subnet" "public_b" {
+    vpc_id = var.vpc_id
+    cidr_block = var.az_b_cidr_public
+    availability_zone = var.az_b 
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-b"
+    }
+}
+
+#-----------------------------------
+# Private App Subnets
+#-----------------------------------
+resource "aws_subnet" "private_app_a" {
+    vpc_id = var.vpc_id
+    cidr_block = var.az_a_cidr_private_app
+    availability_zone = var.az_a
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name = "private-app-subnet-a"
+    }
+}
+
+resource "aws_subnet" "private_app_b" {
+    vpc_id = var.vpc_id
+    cidr_block = var.az_b_cidr_private_app
+    availability_zone = var.az_b
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name = "private-app-subnet-b"
+    }
+}
+
+#-----------------------------------
+# Private DB Subnets
+#-----------------------------------
+resource "aws_subnet" "private_db_a" {
+    vpc_id = var.vpc_id
+    cidr_block = var.az_a_cidr_private_db
+    availability_zone = var.az_a
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name = "private-db-subnet-a"
+    }
+}
+
+resource "aws_subnet" "private_db_b" {
+    vpc_id = var.vpc_id
+    cidr_block = var.az_b_cidr_private_db
+    availability_zone = var.az_b
+    map_public_ip_on_launch = false
+    
+    tags = {
+        Name = "private-db-subnet-b"
+    }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.igw_id
+}
 
