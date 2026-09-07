d5f51a9add36759a8a3a2f6eab6d359954174ba7
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-07 11:52:26 -0400

    Update main.tf

--- project1-3tier-app/modules/s3_logs/main.tf
+++ project1-3tier-app/modules/s3_logs/main.tf
@@ -1,1 +1,48 @@
+resource "aws_s3_bucket" "alb_logs" {
+  bucket = "project1-alb-logs-${random_id.suffix.hex}"
+
+  force_destroy = true
+
+  tags = {
+    Name = "project1-alb-logs"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "alb_logs_policy" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSALBLogs"
+        Effect    = "Allow"
+        Principal = { Service = "logdelivery.elasticloadbalancing.amazonaws.com" }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.alb_logs.arn}/*"
+      }
+    ]
+  })
+}
 
