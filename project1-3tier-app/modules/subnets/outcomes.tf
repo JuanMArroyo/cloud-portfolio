bf4f18b214fddddca290d4799249b45d75a72420
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-07 11:46:18 -0400

    Update outcomes.tf

--- project1-3tier-app/modules/subnets/outcomes.tf
+++ project1-3tier-app/modules/subnets/outcomes.tf
@@ -1,1 +1,17 @@
+output "public_subnet_ids" {
+    value = [
+        aws_subnet.public_a.id,
+        aws_subnet.public_b.id
+    ]
+}
 
+output "private_app_a_id" {
+    value = aws_subnet.private_app_a.id
+}
+
+output "private_db_subnet_ids" {
+    value = [
+        aws_subnet.private_db_a.id,
+        aws_subnet.private_db_b.id
+    ]
+}
