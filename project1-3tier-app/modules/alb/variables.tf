75438742820c9417dc2022436f0fb9fbee56b83c
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-07 11:48:13 -0400

    Add variables for ALB module configuration

--- project1-3tier-app/modules/alb/variables.tf
+++ project1-3tier-app/modules/alb/variables.tf
@@ -1,1 +1,16 @@
+variable "vpc_id" {
+  type = string
+}
 
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "alb_sg_id" {
+  description = "security group ID for the Application Load Balancer"
+  type = string
+}
+
+variable "log_bucket_name" {
+  type = string
+}
