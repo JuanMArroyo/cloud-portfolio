58676da0e5eb5c8d7f428e42692133ea9d94262b
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-07 11:46:01 -0400

    Add variables for VPC and availability zones

--- project1-3tier-app/modules/subnets/variables.tf
+++ project1-3tier-app/modules/subnets/variables.tf
@@ -1,1 +1,40 @@
+variable "vpc_id" {
+    type = string
+}
+
+variable "az_a" {
+    type = string
+}
+
+variable "az_b" {
+    type =string
+}
+
+variable "az_a_cidr_public" {
+    type = string
+}
+
+variable "az_b_cidr_public" {
+    type = string
+}
+
+variable "az_a_cidr_private_app" {
+    type = string
+}
+
+variable "az_b_cidr_private_app" {
+    type = string
+}
+
+variable "az_a_cidr_private_db" {
+    type = string
+}
+
+variable "az_b_cidr_private_db" {
+    type = string
+}
+
+variable "igw_id" {
+  type = string
+}
 
