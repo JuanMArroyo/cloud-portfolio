186dd165615ced6cff6a3a8e21033d965d9b664e
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-07 11:43:23 -0400

    Add VPC and Internet Gateway resources

--- project1-3tier-app/modules/vpc/main.tf
+++ project1-3tier-app/modules/vpc/main.tf
@@ -1,1 +1,18 @@
+resource "aws_vpc" "this" {
+    cidr_block         = var.vpc_cidr
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "project1-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "project1-igw"
+  }
+}
 
