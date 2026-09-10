dfd412f2fa9a96583338b2598025ebac9763f424
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-10 05:06:15 -0400

    Update variables.tf

--- project1-3tier-app/modules/nat/variables.tf
+++ project1-3tier-app/modules/nat/variables.tf
@@ -1,1 +1,20 @@
+variable "project" {
+  type        = string
+  description = "Project name prefix for resource tags"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for NAT Gateway"
+}
+
+variable "public_subnet_id" {
+  type        = string
+  description = "Public subnet ID for NAT Gateway"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private subnet IDs for route table associations"
+}
 
