d265e22ad865d60c5162ad52c9e067c50a7b14a3
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-10 05:08:27 -0400

    Update main.tf

--- project1-3tier-app/modules/vpc_flow_logs/main.tf
+++ project1-3tier-app/modules/vpc_flow_logs/main.tf
@@ -1,1 +1,38 @@
+resource "aws_cloudwatch_log_group" "vpc_flow" {
+  name              = "/aws/vpc/flowlogs"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "vpc_flow_role" {
+  name = "vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc_flow_policy" {
+  role = aws_iam_role.vpc_flow_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = ["logs:CreateLogStream", "logs:PutLogEvents"]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_flow_log" "vpc" {
+  log_destination      = aws_cloudwatch_log_group.vpc_flow.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_role.arn
+  traffic_type         = "ALL"
+  vpc_id               = var.vpc_id
+}
 
