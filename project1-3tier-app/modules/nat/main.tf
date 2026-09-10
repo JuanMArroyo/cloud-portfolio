ab28e320d1bbf0033686695925a66b830948d546
Juan Arroyo <juanm.arroyo312@gmail.com>
2026-09-10 05:05:07 -0400

    Add NAT gateway and associated resources

--- project1-3tier-app/modules/nat/main.tf
+++ project1-3tier-app/modules/nat/main.tf
@@ -1,1 +1,36 @@
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.project}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = var.public_subnet_id   # ← this line fixes the error
+
+  tags = {
+    Name = "${var.project}-nat-gateway"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "${var.project}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  for_each = toset(var.private_subnet_ids)
+
+  subnet_id      = each.value
+  route_table_id = aws_route_table.private_rt.id
+}
 
